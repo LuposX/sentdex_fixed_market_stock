a2e488be951d077878f4f90b0caf1aee3a8a370c
LuposX <36456825+LuposX@users.noreply.github.com>
2019-08-14 01:21:34 +0200

    Update sentdex_visualization_episode13.py

--- source/sentdex_visualization_episode13.py
+++ source/sentdex_visualization_episode13.py
@@ -7,49 +7,49 @@ import seaborn as sns
 from sklearn import svm, preprocessing
 sns.set_style("darkgrid")
 
- #--------------------------------------------------------------------------------------------------------------------------
-def Build_Data_Set(features=["DE Ratio", 
+#--------------------------------------------------------------------------------------------------------------------------
+def Build_Data_Set(features=["DE Ratio",
                              "Trailing P/E"]):
-    
+
     # reads the csv to save it in ram
     data_df = pd.read_csv("Stock_market_full.csv")
     data_df.dropna(axis=0, inplace=True)
-    
+
     # uncomment this if you want to use less data
     # data_df = data_df[:100]
-    
+
     # converts our wished features to a 2d list
     X = np.array(data_df[features].values)
-    
-    # replaces our status thorught 0 or 1 | needs to be binar to use it 
+
+    # replaces our status thorught 0 or 1 | needs to be binar to use it
     y= (data_df["Status"]
         .replace("underperform", 0)
         .replace("outperform", 1)
         .values)
-    
+
     # normalization our data 
     X = preprocessing.scale(X)
-    
+
     return X, y
 
  #--------------------------------------------------------------------------------------------------------------------------
 def Analyis():
     # recall the Build_Data_Set() function to get X, y
     X, y = Build_Data_Set()
-    
+
     # build our svm model
     clf = svm.SVC(kernel="linear", C=1.0)
-    
+
     # trains our model
     clf.fit(X, y)
-    
+
     # variables to visualize our model
     w = clf.coef_[0]
     a = -w[0] / w[1]
-    
-    xx = np.linspace(min(X[:, 0]), max(X[:, 0])) # "X[:, 0]" the 0 stands for the axis first axis 
+
+    xx = np.linspace(min(X[:, 0]), max(X[:, 0])) # "X[:, 0]" the 0 stands for the axis first axis
     yy = a * xx - clf.intercept_[0] / w[1]
-      
+
     return xx, yy, X, y
 
 #--------------------------------------------------------------------------------------------------------------------------
