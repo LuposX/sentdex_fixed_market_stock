4aa3ef55e278e763335a856adab42546e5a727fd
LuposX <36456825+LuposX@users.noreply.github.com>
2019-08-14 01:17:18 +0200

    Update sentdex_prediction_episode15.py

--- source/sentdex_prediction_episode15.py
+++ source/sentdex_prediction_episode15.py
@@ -48,63 +48,63 @@ FEATURES =  ['DE Ratio',
 #-------------------------------------------------------------------------------------------------------
 # example from the video for a function that randomizes our data
 def randomizer():
-    df = pd.DataFrame({"D1": range(5), "D2": range(5)}) 
+    df = pd.DataFrame({"D1": range(5), "D2": range(5)})
     df = df.reindex(np.random.permutation(df.index))
- 
+
 #-------------------------------------------------------------------------------------------------------
 def Build_Data_Set():
-    
+
     # reads the csv to save it in ram. Make sure to chaneg the path when you use it
     data_df = pd.read_csv("../datasets/Stock_market_full.csv")
     data_df.dropna(axis=0, inplace=True)
-    
+
     # data_df = data_df[:100]
-    
+
     # shuffles our data. You can use the sklearn dunction "shuffle" or the function from "sentdex"
     #data_df = data_df.reindex(np.random.permutation(data_df.index))
     data_df = sklearn.utils.shuffle(data_df)
-    
+
     # converts our wished features to a 2d list
     X = np.array(data_df[FEATURES].values.tolist())
-    
-    # replaces our status thorught 0 or 1 | needs to be binar to use it 
+
+    # replaces our status thorught 0 or 1 | needs to be binar to use it
     y = (data_df["Status"]
         .replace("underperform", 0)
         .replace("outperform", 1)
         .values)
-    
+
     # preprocessing your data | normalization
     X = preprocessing.scale(data_df[FEATURES])
 
     # prints the standart derivation should be close to 1 after normalization
     print("X std: ", X.std())
-    
+
     return X, y
 
 #-------------------------------------------------------------------------------------------------------
 def Analyis(report=False):
-    
+
     # size of our test data set. You can play around with that.
     test_size = 500
-    
+
     # recall the Build_Data_Set() function to get X, y
     X, y = Build_Data_Set()
     print("len of x: ", len(X))
     print(" ")
     #print(X)
-    
+
     # build our svm model
     clf = svm.SVC(kernel="linear", C=1.0)
-    
+
     # trains our model
     clf.fit(X[:-test_size], y[:-test_size]) # [:-test_size] we are leaving the last 500 exampels out for testing later
-      
+
     # predict our training exmaples
     pred = clf.predict(X[-test_size:])
-    
+
     #prints classification report
     if report: print(classification_report(y[-test_size:], pred))
- 
+
     return X, y
 
 #-------------------------------------------------------------------------------------------------------
