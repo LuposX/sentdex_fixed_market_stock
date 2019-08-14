1597b191012d9c7a0033b292ea76ce1282cb8d3f
LuposX <36456825+LuposX@users.noreply.github.com>
2019-08-14 19:51:49 +0200

    Update sentdex_prediction_episode19.py

--- source/sentdex_prediction_episode19.py
+++ source/sentdex_prediction_episode19.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import sklearn
 from sklearn import svm, preprocessing
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score
 
 FEATURES =  ['DE Ratio',
              'Trailing P/E',
@@ -48,53 +48,53 @@ def Build_Data_Set():
     # data_df.dropna(axis=0, inplace=True)
     
     # data_df = data_df[:100]
-    
+
     # shuffles our data
     data_df = sklearn.utils.shuffle(data_df)
-    
+
     # replaces the n/a in dataframe
     data_df = data_df.fillna(0)
-    
+
     # converts our wished features to a 2d list
     X = np.array(data_df[FEATURES].values.tolist())
-    
-    # replaces our status thorught 0 or 1 | needs to be binar to use it 
+
+    # replaces our status thorught 0 or 1 | needs to be binar to use it
     y = (data_df["Status"]
         .replace("underperform", 0)
         .replace("outperform", 1)
         .values)
-    
+
     # preprocessing your data | normalization
     X = preprocessing.scale(data_df[FEATURES])
 
     print("X std: ", X.std())
-    
+
     return X, y
 
 def Analyis(report=False):
-    
+
     # how big our test size is (number of examples)
     test_size = 1000
-    
+
     # call the Build_Data_Set() function to get X, y
     X, y = Build_Data_Set()
     print("len of x: ", len(X))
     print(" ")
     #print(X)
-    
+
     # build our svm model
     clf = svm.SVC(kernel="linear", C=1.0)
-    
+
     # trains our model
     clf.fit(X[:-test_size], y[:-test_size]) # [:-test_size] we are leaving the last 500 exampels out for testing later
-      
+
     # predict our training exmaples
     pred = clf.predict(X[-test_size:])
-    
+
     #prints classification report
     if report: print(classification_report(y[-test_size:], pred))
     if report: print("Model accuracy: ", accuracy_score(y[-test_size:], pred))
-        
+
     return X, y
 
-X, y = Analyis(report=True) 
+X, y = Analyis(report=True)
