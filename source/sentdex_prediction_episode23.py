62b1b2f36f1e8ed7fbdde884e53c33efb7f0c48b
LuposX <36456825+LuposX@users.noreply.github.com>
2019-08-15 23:33:10 +0200

    Update sentdex_prediction_episode23.py
    remove trailing whitespace

--- source/sentdex_prediction_episode23.py
+++ source/sentdex_prediction_episode23.py
@@ -54,60 +54,56 @@ FEATURES =  ['DE Ratio',
 
 
 def Build_Data_Set():
-    
+
     # reads the csv to save it in ram
     data_df = pd.read_csv("Stock_market_acc_NO_NA.csv")
     # data_df.dropna(axis=0, inplace=True)
-    
-    # data_df = data_df[:100]
-    
+
     # shuffles our data
     data_df = sklearn.utils.shuffle(data_df)
-    
+
     # replaces the n/a in dataframe
     data_df = data_df.fillna(0)
-    
+
     # converts our wished features to a 2d list
     X = np.array(data_df[FEATURES].values.tolist())
-    
+
     # replaces our status thorught 0 or 1 | needs to be binar to use it 
     y = (data_df["Status"]
         .replace("underperform", 0)
         .replace("outperform", 1)
         .values)
-    
+
     # preprocessing your data | normalization
     X = preprocessing.scale(data_df[FEATURES])
     scaler = preprocessing.StandardScaler()
     X = scaler.fit_transform(X)
-    
+
     z = np.array(data_df[["stock_p_change", "sp500_p_change"]])
-    
-    print("X std: ", X.std())
-    
+
     return X, y, z
 
 
 def Analyis(report=False):
-    
+
     # how big our test size is (number of examples)
     test_size = 500
-    
+
     # how much we invest in the stocks
     invest_amount = 10000
     total_invest = 0
     if_market = 0
     if_strat = 0
-    
+
     # call the Build_Data_Set() function to get X, y
     X, y, z = Build_Data_Set()
     print("len of x: ", len(X))
+    print("X std: ", X.std()) # should be close to 1.0 after normalization
     print("--------------------------------------------------------------------------")
-    #print(X)
-    
+
     # build our svm model
     clf = svm.SVC(kernel="linear", C=1.0)
-    
+
     # trains our model
     clf.fit(X[:-test_size], y[:-test_size]) # [:-test_size] we are leaving the last 500 exampels out for testing later
 
@@ -119,57 +115,44 @@ def Analyis(report=False):
             total_invest += 1
             if_market += market_return
             if_strat += invest_return
-                                             
+
     # predict our training exmaples
     pred = clf.predict(X[-test_size:])
-    
+
     #prints classification report
     if report: print(classification_report(y[-test_size:], pred))
     if report: print("--------------------------------------------------------------------------")
     if report: print("Model accuracy: ", accuracy_score(y[-test_size:], pred))
-    #if report: print(" ")
-    # if report: print("Confusing matrix: ")
-    # if report: print(confusion_matrix(y[-test_size:], pred))
-    
-    # prints How much profit we would make  
-#     print("Total Trades: ", total_invest)
-#     print("Ending with strategy: ", if_strat)
-#     print("Ending with Market: ", if_market)
-                                             
+
     compared = round(((if_strat - if_market) / if_market * 100.0), 3)
     do_nothing = total_invest * invest_amount
-    
+
     avg_market = round(((if_market - do_nothing) / do_nothing) * 100.0, 3)
     avg_strat = round(((if_strat - do_nothing) / do_nothing) * 100.0, 3)
-    
-#     print("Compared to market, we earn: ", str(compared) + "% more")
-#     print("Average investment return: ", str(avg_strat) + "%")
-#     print("Average market return: ", str(avg_market) + "%")    
-    # print("Average investment return: ", )
-    
+
     data_df = pd.read_csv("forward_sample_NO_NA.csv")
     data_df = data_df.fillna(0) 
-      
+
     # converts our wished features to a 2d list
     X = np.array(data_df[FEATURES].values.tolist())
-    
+
     X = preprocessing.scale(data_df[FEATURES])
-    
+
     z = data_df["Ticker"].values.tolist()
-    
+
     invest_list = []
-    
+
     for i in range(len(X)):
         p = clf.predict([X[i]])[0]
         if p == 1:
             #print("z[i]: ", z[i])
             invest_list.append(z[i])
-    
+
     print("len invest_list: ", len(invest_list), "of", data_df["Ticker"].count())
     if report: print("--------------------------------------------------------------------------")
     print("invest_list: ")
     print(invest_list)
-    
+
     return X, y
-	
-X, y = Analyis(report=True) 
+
+X, y = Analyis(report=True)
